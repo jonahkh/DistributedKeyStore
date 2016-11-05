67f2c49a3b98d75a02b8be8922147cbc39d5ed81
jonah howard <jonahkh@uw.edu>
2016-11-05 16:53:26 -0700

    reverting

--- server/Server.py
+++ server/Server.py
@@ -2,8 +2,6 @@ import argparse
 import logging
 import os
 import sys
-import socket
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from server.RequestManager import RequestManager
 from server.Receiver import ReceiverThread
@@ -15,21 +13,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port', type=int, help='Specify a port for the tcp server to listen')
     args = parser.parse_args()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (socket.gethostbyname(socket.gethostname()), args.port)
-    sock.bind(server_address)
-    sock.listen(1)
-    print(sock.accept())
-    # request_manager = RequestManager(args.port)
-    # receiver = None
-    # try:
-    #     receiver = ReceiverThread(args.port, request_manager)
-    #     receiver.start()
-    #     request_manager.wait_completion()
-    # except KeyboardInterrupt:
-    #     print('terminating program')
-    #     receiver.close_socket()
-    #     os._exit(0)
+    request_manager = RequestManager(args.port)
+    receiver = None
+    try:
+        receiver = ReceiverThread(args.port, request_manager)
+        receiver.start()
+        request_manager.wait_completion()
+    except KeyboardInterrupt:
+        print('terminating program')
+        receiver.close_socket()
+        os._exit(0)
 
 
 if __name__ == "__main__":
