012c06c89014a26b957fef56a1d0604a774a1440
jonah howard <jonahkh@uw.edu>
2016-11-19 18:52:55 -0800

    fixed issues

--- server/Acceptor.py
+++ server/Acceptor.py
@@ -28,12 +28,14 @@ class Acceptor():
         if (self.sequence_number_manager.get_sequence_number() > sequence_number):
             packet = self.packet_manager.get_packet('paxos', 'reject', 'lower sequence number')
             accept = False
+            logger.error('Rejecting proposal {}, sequence number too low {}'.format(data, self.packet_manager.get_time_stamp()))
         else:
             self.sequence_number_manager.set_highest_value('hello', 'world', 'operation')
             packet = self.packet_manager.get_packet('paxos', 'promise', {'sequence_number': self.sequence_number_manager.highest_proposal_number,
                                                                          'value': self.sequence_number_manager.highest_proposed_value})
             self.sequence_number_manager.set(sequence_number)
             self.sequence_number_manager.set_highest_proposed_number(sequence_number)
+            logger.error('Accepting proposal {} {}'.format(data, self.packet_manager.get_time_stamp()))
         self.connection.sendall(packet)
         return accept
 
@@ -41,11 +43,14 @@ class Acceptor():
         response = self.connection.recv(BUFFER_SIZE).decode()
         sequence_number = response['data']
         if (response['status'] == 'reject'):
+            logger.error('Accept message rejected by leader {}'.format(self.packet_manager.get_time_stamp()))
             return False
         elif sequence_number < self.sequence_number_manager.highest_proposal_number():
+            logger.error('Accept message rejected {} sequence number too low {}'.format(response, self.packet_manager.get_time_stamp()))
             self.connection.sendall(self.packet_manager.get_packet('paxos', 'reject', 'sequence number too low'))
             return False
         else:
+            logger.error('Accept message received {} {}'.format(response, self.packet_manager.get_time_stamp()))
             key = response['data']['key']
             value = response['data']['value']
             operation = response['operation']
@@ -56,7 +61,8 @@ class Acceptor():
 
     def __commit(self, data):
         response = self.connection.recv(BUFFER_SIZE).decode()
-        if (response['status']['commit']):
+        if (response['status'] == 'commit'):
+            logger.error('Commit received {}, writing to key store {}'.format(response, self.packet_manager.get_time_stamp()))
             data = response['data']
             key = data['key']
             value = data['value']
@@ -66,22 +72,5 @@ class Acceptor():
             else:
                 self.key_store.delete(key)
             self.sequence_number_manager.set_highest_value()
-
-    # def __handle_2PC(self, connection, client_address):
-    #     ack_packet = self.packet_manager.get_packet('2pc', 'ack', 'waiting for commit')
-    #     logger.error('Sending acknowledgment {} to {} {}'.format(ack_packet, client_address[0], self.packet_manager.get_time_stamp()))
-    #     connection.sendall(ack_packet)
-    #     if (client_address[0] != self.server_address):  # Only acknowledge own request, don't commit write (client address on calling thread)
-    #         response = connection.recv(BUFFER_SIZE).decode()
-    #         logger.error('Received response {} from: {}'.format(response, client_address))
-    #         commit_message = json.loads(response)
-    #         if (self.packet_manager.is_valid_2pc_packet(commit_message)):   # Execute if successful commit message (all servers responsive)
-    #             if (commit_message['status'] == 'success'):
-    #                 logger.error('Commit message received {} from {} {}'.format(commit_message, client_address[0], self.packet_manager.get_time_stamp()))
-    #                 self.__get_data(commit_message)
-    #             else:
-    #                 logger.error('Aborting request from {}:{} {}'.format(client_address[0], client_address[1],
-    #                                                                       self.packet_manager.get_time_stamp()))
-    #         else:
-    #             logger.error('Received malformed request from {}: {} {}'.format(client_address[0], client_address[1],
-    #                                                                   self.packet_manager.get_time_stamp()))
+        else:
+            logger.error('Commit rejected {} {}'.format(response, self.packet_manager.get_time_stamp()))
