4f8e6737884b7f4ba44c684abbda0f83345e53b4
jonah howard <jonahkh@uw.edu>
2016-11-19 18:45:18 -0800

    fixed issues

--- server/Proposer.py
+++ server/Proposer.py
@@ -22,20 +22,24 @@ class Proposer():
         self.server_address = server_address
         self.server_addresses = server_addresses
         self.port = port
+        self.accept_count = 0
 
     def propose(self, data):
         sequence_number = self.sequence_number.increment()
         key = data['data']['key']
         value = data['data']['value']
         operation = data['operation']
-        commit_message = True
         response_list, request_list = self.__prepare_propose_commit(sequence_number, key, value, operation)
-        acceptors = None
         if len(request_list) < QUORUM:
-            self.__accept(response_list, key, value, operation)
+            accept_list, highest_value = self.__accept(response_list, key, value, operation)
+            if (self.accept_count > QUORUM):
+                packet = self.packet_manager.get_packet('paxos', 'commit', {'key': highest_value['key'], 'value': highest_value['value']}, highest_value['operation'])
+                self.__send_commit(packet, accept_list)
         else:
             logger.error('Quorum not received, rejecting promises')
         return self.packet_manager.get_packet('tcp', 'success', 'success')
+
+
     # Phase 1
     def __prepare_propose_commit(self, sequence_number, key, value, operation):
         request_list = copy.copy(self.server_addresses)
@@ -52,11 +56,6 @@ class Proposer():
             logger.error('failed to connect to server {}'.format(e))
         except Exception as e:
             print(e)
-
-        # packet = self.packet_manager.get_packet('2pc', 'success', {'key': key, 'value': value}, operation) \
-        #     if not request_list else self.packet_manager.get_packet('2pc', 'failure', 'abort')
-        # self.__send_commit(packet, response_list)
-        # return not request_list
         return response_list, request_list
 
     def __propose_commit(self, server_address, request_list, sequence_number):
@@ -82,7 +81,7 @@ class Proposer():
 
         if (msg['status'] == 'promise'):
             request_list.remove(server_address)
-        return sock, msg
+        return sock, msg, server_address
 
     # Phase 2
     def __accept(self, response_list, key, value, operation):
@@ -103,11 +102,8 @@ class Proposer():
                     value_count[data['value']['value']] = 1
                 else:
                     value_count[data['value']] = 1
-        print('values: {}'.format(values))
-        print('value count: {}'.format(value_count))
         highest_value = values[0]
         highest_count = value_count[values[0]['value']]
-        print('Highest value: {}'.format(highest_value))
         for val in values:
             val = val['value']
             if (value_count[val] > highest_count):
@@ -120,35 +116,32 @@ class Proposer():
             packet = self.packet_manager.get_packet('paxos', 'accept', {'key': highest_value['key'], 'value': highest_value['value']}, highest_value['operation'])
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
             for response in response_list:
-                accept_list.append(executor.submit(self.__send_accept, response.result()[0], packet))
+                accept_list.append(executor.submit(self.__send_accept, response.result()[0], response.result()[2], packet))
+            beg_time = time.time()
+            while self.accept_count < QUORUM and time.time() - beg_time < 1:  # Timeout after 5 seconds
+                pass
+            return accept_list, highest_value
 
-
-    def __send_accept(self, client_address, packet):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        count = 0
-        sock.settimeout(.5)
-        print(packet)
-        while True:
-            try:
-                sock.connect((server_address, self.port))
-                break
-            except:
-                count += 1
-                if (count > 5): # timeout after 5 tries
-                    sock.close()
-                    return
-        packet = self.packet_manager.get_packet('paxos', 'prepare commit', sequence_number)
-        logger.error('Sending prepare commit {} to {}'.format(packet, server_address))
+    def __send_accept(self, sock, client_address, packet):
+        logger.error('Sending accept ok {} to {}'.format(packet, client_address))
         sock.sendall(packet)
         msg = sock.recv(BUFFER_SIZE).decode()
-        logger.error('Promise {} received from {}'.format(msg, server_address))
+        logger.error('Accept {} received from {}'.format(msg, client_address))
+        if (isinstance(msg, str)):
+            msg = json.loads(msg)
+        if (msg['status'] == 'accept'):
+            self.accept_count += 1
+            return client_address
+        else:
+            sock.close()
 
     # Phase 3
     def __send_commit(self, packet, response_list):
-        for response in response_list:
-            sock = response.result()
-            if (sock):
-                peer_name = sock.getpeername()
-                if (self.server_address != peer_name[0]):
-                    sock.sendall(packet)
-                sock.close()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            for response in response_list:
+                executor.submit(self.__commit, response[0], response[2], packet)
+
+    def __commit(self, sock, client_address, packet):
+        logger.error('Sending commit ok {} to {}'.format(packet, client_address))
+        sock.sendall(packet)
+        sock.close()
